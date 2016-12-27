573b34e396ebef36b06fe479452280ca39752970
Buğra Ekuklu <ekuklu@icloud.com>
2016-12-27 03:17:01 +0200

    FIX(2FA): Indentation fault.

--- web/models/one_time_code.ex
+++ web/models/one_time_code.ex
@@ -11,7 +11,7 @@ defmodule Spherium.OneTimeCode do
     field :inserted_at, :naive_datetime
   end
 
-  def changeset(struct, params \\ %{}) do
+  def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:user_id, :code, :device, :user_agent])
     |> validate_required([:user_id, :code, :device, :user_agent])
