ed31f0c4f63b62e26bdc5af51c9c9c765a81ce45
Buğra Ekuklu <ekuklu@icloud.com>
2016-12-27 22:26:20 +0300

    FIX(2FA): Removed redundant unban feature

--- web/controllers/access_control/authentication/one_time_code_controller.ex
+++ web/controllers/access_control/authentication/one_time_code_controller.ex
@@ -12,6 +12,7 @@ defmodule Spherium.OneTimeCodeController do
   alias Spherium.Passphrase
   alias Spherium.PassphraseInvalidation
   alias Spherium.OneTimeCodeInvalidation
+  alias Spherium.Unban
 
   plug :scrub_params, "credentials" when action in [:create]
 
@@ -58,14 +59,6 @@ defmodule Spherium.OneTimeCodeController do
 
             if Repo.aggregate(query, :count, :id) == 2, do: Repo.rollback(:otc_quota_reached)
 
-            query = from otc in OneTimeCode,
-                    left_join: p in Passphrase, on: p.one_time_code_id == otc.id,
-                    where: otc.user_id == ^user.id and
-                           otc.inserted_at > ago(8, "hour") and
-                           is_nil(p.inserted_at)
-
-            if Repo.aggregate(query, :count, :id) == 4, do: Repo.rollback(:possible_auth_probing)
-
             one_time_code_changeset =
               OneTimeCode.changeset(
                 %OneTimeCode{},
@@ -85,18 +78,23 @@ defmodule Spherium.OneTimeCodeController do
 
           conn
           |> put_status(:created)
+          |> put_resp_content_type("application/json")
           |> render("one_time_code.json", one_time_code: one_time_code)
         {:error, :max_passphrases_reached} ->
-          send_resp(conn, :conflict, "Maximum number of passphrases available is reached (5).")
+          conn
+          |> put_resp_content_type("application/json")
+          |> send_resp(:conflict, "Maximum number of passphrases available is reached (5).")
         {:error, :otc_quota_reached} ->
-          send_resp(conn, :too_many_requests, "OTC quota per 15 minutes is reached (2).")
-        {:error, :possible_auth_probing} ->
-          send_resp(conn, :too_many_requests, "Authentication probing probability detected.")
+          conn
+          |> put_resp_content_type("application/json")
+          |> send_resp(:too_many_requests, "OTC quota per 15 minutes is reached (2).")
         {:error, :invalid_password} ->
           attempt_changeset
           |> Repo.insert!()
 
-          send_resp(conn, :forbidden, "Invalid username/password combination.")
+          conn
+          |> put_resp_content_type("application/json")
+          |> send_resp(:forbidden, "Invalid username/password combination.")
       end
     else
       conn
