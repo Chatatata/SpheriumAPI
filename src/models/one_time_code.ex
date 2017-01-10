17baf31840a7542af62188fbc3717d86a5d38b63
Buğra Ekuklu <ekuklu@icloud.com>
2017-01-10 04:54:01 +0200

    OTC became subsidiary entity again.

--- src/models/one_time_code.ex
+++ src/models/one_time_code.ex
@@ -1,21 +1,18 @@
 defmodule Spherium.OneTimeCode do
   use Spherium.Web, :model
 
-  @uuid_regex ~r|[0-9,a-z,A-Z]{8}-[0-9,a-z,A-Z]{4}-[0-9,a-z,A-Z]{4}-[0-9,a-z,A-Z]{4}-[0-9,a-z,A-Z]{12}|
-
   schema "one_time_codes" do
     belongs_to :user, Spherium.User
     field :code, :integer
-    field :device, Ecto.UUID
-    field :user_agent, :string
     field :inserted_at, :naive_datetime
+
+    has_one :passphrase, Spherium.Passphrase
   end
 
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:user_id, :code, :device, :user_agent])
-    |> validate_required([:user_id, :code, :device, :user_agent])
-    |> validate_format(:device, @uuid_regex)
+    |> cast(params, [:user_id, :code])
+    |> validate_required([:user_id, :code])
     |> foreign_key_constraint(:user_id)
   end
 end
