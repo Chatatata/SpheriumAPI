a2c94417414d5a37a0bd32908f1960453dbdd1b4
Buğra Ekuklu <ekuklu@icloud.com>
2017-01-16 04:03:34 +0200

    Reflected model changes.

--- test/models/passphrase_test.exs
+++ test/models/passphrase_test.exs
@@ -2,14 +2,65 @@ defmodule Spherium.PassphraseTest do
   use Spherium.ModelCase
 
   alias Spherium.Passphrase
+  alias Spherium.Passkey
   alias Spherium.Factory
 
   test "changeset with valid attributes" do
     user = Factory.insert(:user)
+
+    changeset =
+      Passphrase.changeset(%Passphrase{},
+                           %{passkey: Passkey.generate(),
+                           user_agent: "ExUnit",
+                           user_id: user.id,
+                           device: Ecto.UUID.generate()})
+
+    assert changeset.valid?
+  end
+
+  test "changeset without user identifier" do
+    changeset =
+      Passphrase.changeset(%Passphrase{},
+                           %{passkey: Passkey.generate()})
+
+    refute changeset.valid?
+  end
+
+  test "changeset without device identifier" do
+    user = Factory.insert(:user)
+
+    changeset =
+      Passphrase.changeset(%Passphrase{},
+                           %{passkey: Passkey.generate(),
+                           user_agent: "ExUnit",
+                           user_id: user.id})
+
+    refute changeset.valid?
+  end
+
+  test "changeset without user agent" do
+    user = Factory.insert(:user)
+
+    changeset =
+      Passphrase.changeset(%Passphrase{},
+                           %{passkey: Passkey.generate(),
+                           device: Ecto.UUID.generate(),
+                           user_id: user.id})
+
+    refute changeset.valid?
+  end
+
+  test "changeset with one time code identifier" do
+    user = Factory.insert(:user, authentication_scheme: :two_factor_over_otc)
     otc = Factory.insert(:one_time_code, user_id: user.id)
 
-    changeset = Passphrase.changeset(%Passphrase{}, %{one_time_code_id: otc.id,
-                                                      passkey: Ecto.UUID.generate()})
+    changeset =
+      Passphrase.changeset(%Passphrase{},
+                           %{passkey: Passkey.generate(),
+                             user_id: user.id,
+                             device: Ecto.UUID.generate(),
+                             user_agent: "ExUnit",
+                             one_time_code_id: otc.id})
 
     assert changeset.valid?
   end
@@ -18,13 +69,17 @@ defmodule Spherium.PassphraseTest do
     user = Factory.insert(:user)
     otc = Factory.insert(:one_time_code, user_id: user.id)
 
-    changeset = Passphrase.changeset(%Passphrase{}, %{one_time_code_id: otc.id})
+    changeset =
+      Passphrase.changeset(%Passphrase{},
+                           %{one_time_code_id: otc.id})
 
     refute changeset.valid?
   end
 
   test "changeset without one time code identifier" do
-    changeset = Passphrase.changeset(%Passphrase{}, %{passkey: Ecto.UUID.generate()})
+    changeset =
+      Passphrase.changeset(%Passphrase{},
+                           %{passkey: Passkey.generate()})
 
     refute changeset.valid?
   end
