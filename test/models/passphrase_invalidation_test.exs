603912121e2eaaebe9264297b146aa34906331bd
Buğra Ekuklu <ekuklu@icloud.com>
2017-01-16 05:01:34 +0200

    Added regression tests and fixed failures.

--- test/models/passphrase_invalidation_test.exs
+++ test/models/passphrase_invalidation_test.exs
@@ -5,11 +5,9 @@ defmodule Spherium.PassphraseInvalidationTest do
   alias Spherium.Factory
 
   test "changeset with valid attributes" do
-    user = Factory.insert(:user)
-    target_otc = Factory.insert(:one_time_code, user_id: user.id)
-    target = Factory.insert(:passphrase, one_time_code_id: target_otc.id)
-    source_otc = Factory.insert(:one_time_code, user_id: user.id)
-    source = Factory.insert(:passphrase, one_time_code_id: source_otc.id)
+    user = Factory.insert(:user, authentication_scheme: :two_factor_over_otc)
+    target = Factory.insert(:passphrase, user_id: user.id)
+    source = Factory.insert(:passphrase, user_id: user.id)
 
     changeset =
       PassphraseInvalidation.changeset(
@@ -23,11 +21,44 @@ defmodule Spherium.PassphraseInvalidationTest do
   end
 
   test "changeset without ip address" do
-    user = Factory.insert(:user)
+    user = Factory.insert(:user, authentication_scheme: :two_factor_over_otc)
+    target = Factory.insert(:passphrase, user_id: user.id)
+    source = Factory.insert(:passphrase, user_id: user.id)
+
+    changeset =
+      PassphraseInvalidation.changeset(
+        %PassphraseInvalidation{},
+        %{passphrase_id: source.id,
+          target_passphrase_id: target.id}
+      )
+
+    refute changeset.valid?
+  end
+
+  test "changeset with valid attributes (regression)" do
+    user = Factory.insert(:user, authentication_scheme: :two_factor_over_otc)
     target_otc = Factory.insert(:one_time_code, user_id: user.id)
-    target = Factory.insert(:passphrase, one_time_code_id: target_otc.id)
+    target = Factory.insert(:passphrase, user_id: user.id, one_time_code_id: target_otc.id)
     source_otc = Factory.insert(:one_time_code, user_id: user.id)
-    source = Factory.insert(:passphrase, one_time_code_id: source_otc.id)
+    source = Factory.insert(:passphrase, user_id: user.id, one_time_code_id: source_otc.id)
+
+    changeset =
+      PassphraseInvalidation.changeset(
+        %PassphraseInvalidation{},
+        %{passphrase_id: source.id,
+          target_passphrase_id: target.id,
+          ip_addr: "0.0.0.0"}
+      )
+
+    assert changeset.valid?
+  end
+
+  test "changeset without ip address (regression)" do
+    user = Factory.insert(:user, authentication_scheme: :two_factor_over_otc)
+    target_otc = Factory.insert(:one_time_code, user_id: user.id)
+    target = Factory.insert(:passphrase, user_id: user.id, one_time_code_id: target_otc.id)
+    source_otc = Factory.insert(:one_time_code, user_id: user.id)
+    source = Factory.insert(:passphrase, user_id: user.id, one_time_code_id: source_otc.id)
 
     changeset =
       PassphraseInvalidation.changeset(
