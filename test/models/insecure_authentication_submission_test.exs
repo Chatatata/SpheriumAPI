ec79fd50fb7223423b71e132a4e8e9608d7765f8
Buğra Ekuklu <ekuklu@icloud.com>
2017-01-20 08:00:09 +0300

    FIX: Insecure authentication submission should be deaf.

--- test/models/insecure_authentication_submission_test.exs
+++ test/models/insecure_authentication_submission_test.exs
@@ -2,43 +2,28 @@ defmodule Spherium.InsecureAuthenticationSubmissionTest do
   use Spherium.ModelCase
 
   alias Spherium.InsecureAuthenticationSubmission
-  alias Spherium.Factory
   alias Spherium.Passkey
 
-  setup _context do
-    %{user: Factory.insert(:user)}
-  end
-
-  test "changeset with valid attributes", %{user: user} do
-    changeset =
-      InsecureAuthenticationSubmission.changeset(%InsecureAuthenticationSubmission{},
-                                                 %{user_id: user.id,
-                                                   passkey: Passkey.generate()})
-
-    assert changeset.valid?
-  end
-
-  test "changeset without user identifier", %{user: _user} do
+  test "changeset with valid attributes" do
     changeset =
       InsecureAuthenticationSubmission.changeset(%InsecureAuthenticationSubmission{},
                                                  %{passkey: Passkey.generate()})
 
+    assert changeset.valid?
+  end
+
+  test "changeset without passkey" do
+    changeset =
+      InsecureAuthenticationSubmission.changeset(%InsecureAuthenticationSubmission{},
+                                                 %{})
+
     refute changeset.valid?
   end
 
-  test "changeset without passkey", %{user: user} do
+  test "changeset with invalid passkey" do
     changeset =
       InsecureAuthenticationSubmission.changeset(%InsecureAuthenticationSubmission{},
-                                                 %{user_id: user.id})
-
-    refute changeset.valid?
-  end
-
-  test "changeset with invalid passkey", %{user: user} do
-    changeset =
-      InsecureAuthenticationSubmission.changeset(%InsecureAuthenticationSubmission{},
-                                                 %{user_id: user.id,
-                                                   passkey: "invalid passkey"})
+                                                 %{passkey: "invalid passkey"})
 
     refute changeset.valid?
   end
