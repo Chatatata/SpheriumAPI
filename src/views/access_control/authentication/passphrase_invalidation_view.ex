793d5a12a6f4a9625ec429fcf4329d172ec9c7b0
Buğra Ekuklu <ekuklu@icloud.com>
2017-01-21 09:04:29 +0300

    Removed unused views.

--- src/views/access_control/authentication/passphrase_invalidation_view.ex
+++ src/views/access_control/authentication/passphrase_invalidation_view.ex
@@ -5,18 +5,7 @@ defmodule Spherium.PassphraseInvalidationView do
     %{data: render_one(passphrase_invalidation, Spherium.PassphraseInvalidationView, "passphrase_invalidation.json")}
   end
 
-  def render("show.min.json", %{passphrase_invalidation: passphrase_invalidation}) do
-    %{data: render_one(passphrase_invalidation, Spherium.PassphraseInvalidationView, "passphrase_invalidation.min.json")}
-  end
-
   def render("passphrase_invalidation.json", %{passphrase_invalidation: passphrase_invalidation}) do
-    %{passphrase: render_one(passphrase_invalidation.passphrase, Spherium.PassphraseView, "passphrase.json"),
-      target_passphrase: render_one(passphrase_invalidation.target_passphrase, Spherium.PassphraseView, "passphrase.json"),
-      ip_addr: passphrase_invalidation.ip_addr,
-      timestamp: passphrase_invalidation.inserted_at}
-  end
-
-  def render("passphrase_invalidation.min.json", %{passphrase_invalidation: passphrase_invalidation}) do
     %{ip_addr: passphrase_invalidation.ip_addr,
       timestamp: passphrase_invalidation.inserted_at}
   end
