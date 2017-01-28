f3a596243faa995cb7bc58445f08e2a1ee7d090b
Buğra Ekuklu <ekuklu@icloud.com>
2017-01-28 09:10:32 +0300

    FIX: Indentation.

--- src/policies/access_control/authentication/attempt_controller_policy.ex
+++ src/policies/access_control/authentication/attempt_controller_policy.ex
@@ -1,5 +1,6 @@
 defmodule Spherium.AttemptControllerPolicy do
   def index(conn, _params, :self) do
-    is_binary(conn.query_params["username"]) and conn.query_params["username"] =~ conn.assigns[:user].username
+    is_binary(conn.query_params["username"]) and
+    conn.query_params["username"] =~ conn.assigns[:user].username
   end
 end
