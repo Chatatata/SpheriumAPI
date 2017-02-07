da308fea021e2b49faf88ec74b6447caedb3612f
Buğra Ekuklu <ekuklu@icloud.com>
2017-02-08 00:43:12 +0300

    FIX: Added missing policy entry and unit test.

--- src/policies/access_control/authorization/permission_set_grant_controller_policy.ex
+++ src/policies/access_control/authorization/permission_set_grant_controller_policy.ex
@@ -2,4 +2,8 @@ defmodule Spherium.PermissionSetGrantControllerPolicy do
   def index(conn, _params, :self) do
     is_binary(conn.query_params["target_user_id"]) and conn.query_params["target_user_id"] =~ Integer.to_string(conn.assigns[:user].id)
   end
+
+  def index(_conn, _params, :all) do
+    true
+  end
 end
--- test/policies/access_control/authorization/permission_set_grant_controller_policy_test.exs
+++ test/policies/access_control/authorization/permission_set_grant_controller_policy_test.exs
@@ -4,15 +4,26 @@ defmodule Spherium.PermissionSetGrantControllerPolicyTests do
   alias Spherium.Factory
   alias Spherium.PermissionSetGrantControllerPolicy
 
+  test "user wants to access others permission set grants", %{user: user} do
+    other_user = Factory.insert(:user)
+
+    conn =
+      build_conn(:get, "/?target_user_id=" <> Integer.to_string(other_user.id))
+      |> fetch_query_params()
+      |> assign(:user, user)
+
+    assert PermissionSetGrantControllerPolicy.index(conn, nil, :all)
+  end
+
+  @tag attach_to_one_permissions: true
   test "user wants to access all permission set grants", %{conn: conn} do
     conn = fetch_query_params(conn)
 
     refute PermissionSetGrantControllerPolicy.index(conn, nil, :self)
   end
 
-  test "user wants to access his/her permission set grants", %{conn: conn} do
-    user = conn.assigns[:user]
-
+  @tag attach_to_one_permissions: true
+  test "user wants to access his/her permission set grants", %{user: user} do
     conn =
       build_conn(:get, "/?target_user_id=" <> Integer.to_string(user.id))
       |> fetch_query_params()
@@ -21,8 +32,8 @@ defmodule Spherium.PermissionSetGrantControllerPolicyTests do
     assert PermissionSetGrantControllerPolicy.index(conn, nil, :self)
   end
 
-  test "user wants to access others permission set grants", %{conn: conn} do
-    user = conn.assigns[:user]
+  @tag attach_to_one_permissions: true
+  test "throws when self user wants to access others permission set grants", %{user: user} do
     other_user = Factory.insert(:user)
 
     conn =
