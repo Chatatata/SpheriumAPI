d8966cf51721625aa6d285fd3696b5129720196f
Buğra Ekuklu <ekuklu@icloud.com>
2017-02-07 17:15:23 +0300

    Added unit tests about authorization.

--- src/controllers/access_control/authentication/passphrase_invalidation_controller.ex
+++ src/controllers/access_control/authentication/passphrase_invalidation_controller.ex
@@ -11,43 +11,39 @@ defmodule Spherium.PassphraseInvalidationController do
   def create(conn,
              %{"passphrase_invalidation" =>
                %{"target_passphrase_id" => target_passphrase_id}}) do
-    # Check if user owns target passphrase
     ip_addr =
       conn.remote_ip
       |> Tuple.to_list()
       |> Enum.join(".")
 
-    changeset = PassphraseInvalidation.changeset(%PassphraseInvalidation{},
-                                                 %{passphrase_id: conn.assigns[:passphrase_id],
-                                                   target_passphrase_id: target_passphrase_id,
-                                                   ip_addr: ip_addr})
+    changeset =
+      PassphraseInvalidation.changeset(
+        %PassphraseInvalidation{},
+        %{passphrase_id: conn.assigns[:passphrase_id],
+          target_passphrase_id: target_passphrase_id,
+          ip_addr: ip_addr}
+      )
 
     try do
-      case Repo.transaction(fn ->
-        if conn.assigns[:permission_type] == :self do
-          passphrase = Repo.get!(Passphrase, target_passphrase_id)
+      {:ok, passphrase_invalidation} =
+        Repo.transaction(fn ->
+          if conn.assigns[:permission_type] == :self do
+            passphrase = Repo.get!(Passphrase, target_passphrase_id)
 
-          if passphrase.user_id == conn.assigns[:user].id, do: Repo.rollback(:unauthorized)
-        end
+            validate_possession conn, passphrase
+          end
 
-        Repo.insert!(changeset)
-      end) do
-        {:ok, passphrase_invalidation} ->
-          conn
-          |> put_status(:created)
-          |> render("show.json", passphrase_invalidation: passphrase_invalidation)
-        {:error, :unauthorized} ->
-          raise InsufficientScopeError
-      end
+          Repo.insert!(changeset)
+        end)
+
+      conn
+      |> put_status(:created)
+      |> render("show.json", passphrase_invalidation: passphrase_invalidation)
     rescue
-      Ecto.NoResultsError ->
-        conn
-        |> send_resp(:not_found, "")
       ice in Ecto.InvalidChangesetError ->
         case List.keyfind(ice.changeset.errors, :target_passphrase_id, 0) do
           {:target_passphrase_id, {"does not exist", []}} ->
-            conn
-            |> send_resp(:not_found, "")
+            raise Ecto.NoResultsError, queryable: Passphrase
           _ ->
             conn
             |> put_status(:unprocessable_entity)
--- test/controllers/access_control/authentication/passphrase_invalidation_controller_test.exs
+++ test/controllers/access_control/authentication/passphrase_invalidation_controller_test.exs
@@ -18,14 +18,16 @@ defmodule Spherium.PassphraseInvalidationControllerTest do
   end
 
   test "returns 404 when passphrase does not exist", %{conn: conn} do
-    conn = post conn,
-                authentication_passphrase_invalidation_path(
-                  conn,
-                  :create,
-                  passphrase_invalidation: %{target_passphrase_id: -1}
-                )
-
-    assert conn.status == 404
+    assert_error_sent 404, fn ->
+      post conn,
+           authentication_passphrase_invalidation_path(
+             conn,
+             :create,
+             passphrase_invalidation: %{
+               target_passphrase_id: -1
+             }
+           )
+    end
   end
 
   test "returns 422 when target_passphrase_id is incorrect", %{conn: conn} do
@@ -37,5 +39,39 @@ defmodule Spherium.PassphraseInvalidationControllerTest do
                 )
 
     assert conn.status == 422
+  end
+
+  @tag attach_to_one_permissions: true
+  test "raises when self user attempts to invalidate others passphrase", %{conn: conn} do
+    user = Factory.insert(:user)
+    passphrase = Factory.insert(:passphrase, user: user)
+
+    assert_error_sent 401, fn ->
+      post conn,
+           authentication_passphrase_invalidation_path(
+            conn,
+            :create,
+            passphrase_invalidation: %{
+              target_passphrase_id: passphrase.id
+            }
+           )
+    end
+  end
+
+  @tag attach_to_one_permissions: true
+  test "permits a self user to invalidate own passphrase", %{conn: conn} do
+    user = conn.assigns[:setup_user]
+    passphrase = Factory.insert(:passphrase, user: user)
+
+    conn = post conn,
+           authentication_passphrase_invalidation_path(
+            conn,
+            :create,
+            passphrase_invalidation: %{
+              target_passphrase_id: passphrase.id
+            }
+           )
+
+    assert conn.status == 201
   end
 end
