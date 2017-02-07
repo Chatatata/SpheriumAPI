2609e5192e64240093935587e6d90c410769a9b4
Buğra Ekuklu <ekuklu@icloud.com>
2017-02-08 00:23:09 +0300

    FIX: Added missing unit test.

--- test/policies/access_control/authorization/user_permission_set_controller_policy_test.exs
+++ test/policies/access_control/authorization/user_permission_set_controller_policy_test.exs
@@ -15,4 +15,11 @@ defmodule Spherium.UserPermissionSetControllerPolicyTest do
 
     refute UserPermissionSetControllerPolicy.show(conn, %{"user_id" => Integer.to_string(user.id)}, :self)
   end
+
+  test "user mistypes user identifier", %{conn: conn} do
+    assert_raise Spherium.PolicyPrehookParameterValidationError,
+                 ~r/could not perform policy prehook because changeset is invalid./, fn ->
+      UserPermissionSetControllerPolicy.show(conn, %{"user_id" => ~s(invalid)}, :self)
+    end
+  end
 end
